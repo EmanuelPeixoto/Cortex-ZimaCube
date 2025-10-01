1e3aa7d1c25fc676377fd3d8a69e16bf41118087
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-10-01 17:48:26 -0300

    fixed sftpgo permissions

--- system/sftpgo.nix
+++ system/sftpgo.nix
@@ -12,16 +12,18 @@
         bindings = [{ port = 2022; address = "0.0.0.0"; }];
       };
       data_provider = {
-        # Usando o banco de dados padrão, que é suficiente para começar
         name = "sqlite";
-        # O NixOS gerencia o path automaticamente
+        default_user_permissions = [ "*" ];
       };
-      common = {
-        # O SFTPGo criará as homes dos usuários de sistema aqui
-        # Esta diretiva não é necessária se os usuários já existem no sistema
-        # A home já é definida no módulo `users.users` em alunos.nix
-      };
+      common = {};
     };
   };
-  networking.firewall.allowedTCPPorts = [ 8080 ];
+
+  systemd.services.sftpgo = {
+    serviceConfig = {
+      ReadWritePaths = [ "/mnt/nextcloud" ];
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8080 2022 ];
 }

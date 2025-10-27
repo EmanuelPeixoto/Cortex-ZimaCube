c584e2e3f2e45497aabbc50d44676aca9b9f0d88
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-10-27 19:00:38 -0300

    changed HD

--- system/HDs.nix
+++ system/HDs.nix
@@ -1,6 +1,6 @@
 {
   fileSystems."/mnt/sharefiles" = {
-    device = "/dev/disk/by-uuid/00414c9c-890a-4b03-88a9-b893cf702e9f";
+    device = "/dev/disk/by-uuid/514647c3-0012-4b51-8623-e9bed31a6a85";
     fsType = "ext4";
     options = [ "acl" ];
   };

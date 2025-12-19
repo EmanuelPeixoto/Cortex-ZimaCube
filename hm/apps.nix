b5eae3d7f6f39c1676e1131627d0c51498033aac
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-12-19 11:08:53 -0300

    removed unsued apps

--- hm/apps.nix
+++ hm/apps.nix
@@ -10,30 +10,24 @@ in
     motd
     yt-tlp-menu
 
-    audacity                  # Sound editor with graphical UI
     curl                      # Command line tool for transferring files with URL syntax
     exfatprogs                # exFAT filesystem userspace utilities
     ffmpeg                    # Complete, cross-platform solution to record, convert and stream audio and video
     gcc                       # GNU Compiler Collection
     gdb                       # GNU Project debugger
     gh                        # GitHub CLI tool
-    gimp                      # GNU Image Manipulation Program
     glibc                     # GNU C Library
     gnumake                   # Tool to control the generation of non-source files from sources
     gparted                   # Graphical disk partitioning tool
     iftop                     # Display bandwidth usage on a network interface
     inetutils                 # Collection of common network programs
     iw                        # Tool to use nl80211
-    jmtpfs                    # FUSE filesystem for MTP devices like Android phones
     killall                   # No description :/
     lazygit                   # Simple terminal UI for git commands
-    libmtp                    # Implementation of Microsoft's Media Transfer Protocol
-    libreoffice               # Comprehensive, professional-quality productivity suite, a variant of openoffice.org
     lm_sensors                # Tools for reading hardware sensors
     lshw                      # Provide detailed information on the hardware configuration of the machine
     ncdu                      # Disk usage analyzer with an ncurses interface
     nload                     # Monitors network traffic and bandwidth usage with ncurses graphs
-    nomacs                    # Qt-based image viewer
     pciutils                  # Collection of programs for inspecting and manipulating configuration of PCI devices
     progress                  # Tool that shows the progress of coreutils programs
     smartmontools             # Tools for monitoring the health of hard drives
@@ -45,7 +39,6 @@ in
     wget                      # Tool for retrieving files using HTTP, HTTPS, and FTP
     wl-clipboard              # Command-line copy/paste utilities for Wayland
     yt-dlp                    # Command-line tool to download videos from YouTube.com and other sites (youtube-dl fork)
-    zathura                   # Highly customizable and functional PDF viewer
     zip                       # Compressor/archiver for creating and modifying zipfiles
   ];
 }

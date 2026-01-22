0ab9d5aecbaeec2ce195d09d63828399eca1beda
EmanuelPeixoto <leunamepeixoto@gmail.com>
2026-01-22 13:58:59 -0300

    removed www user

--- system/users.nix
+++ system/users.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   users = {
     users = {
@@ -7,22 +7,6 @@
         isNormalUser = true;
         description = "ZimaCube";
         extraGroups = [ "networkmanager" "wheel" ];
-      };
-
-      www = {
-        shell = pkgs.fish;
-        isSystemUser = true;
-        createHome = true;
-        home = "/home/www";
-        group = "www";
-        extraGroups = [ "nextcloud" "wheel" ];
-      };
-    };
-
-    groups = {
-      www = {
-        name = "www";
-        members = [ "nginx" "nextcloud" ];
       };
     };
   };

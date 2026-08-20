193f11e35bae9e12ac2c743a5271c6b4e1178d96
EmanuelPeixoto <leunamepeixoto@gmail.com>
2026-08-20 10:40:00 -0300

    updated flake

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "My NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
@@ -17,7 +17,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lexis.url = "github:EmanuelPeixoto/Lexis";
-    llm-agents.url = "github:numtide/llm-agents.nix";
+    llm-agents = {
+      url = "github:numtide/llm-agents.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
@@ -47,7 +50,6 @@
           inherit pkgs;
           modules = [
             ./system
-            home-manager.nixosModules.home-manager
           ];
           specialArgs = { inherit inputs; };
         };
